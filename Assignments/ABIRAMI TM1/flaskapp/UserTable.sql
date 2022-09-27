4dfb7da75aa42387ce534009eb7036bdf4843b86
winsongr <winsongr08@gmail.com>
2022-09-27 16:28:24 +0530

    fix

---
+++ Assignments/ABIRAMI TM1/flaskapp/UserTable.sql
@@ -0,0 +1,19 @@
+CREATE TABLE USER (
+    RollNumber varchar(10) NOT NULL PRIMARY KEY,
+    Email varchar(255) NOT NULL,
+    Username varchar(255) NOT NULL UNIQUE,
+    Password varchar(255) NOT NULL
+);
+
+INSERT INTO USER VALUES ('710019104002', 'abi@gmail.com', 'abi', 'abi@123');
+SELECT * FROM USER;
+
+-- 
+UPDATE USER SET Email='abirami@gmail.com' WHERE RollNumber='710019104002';
+SELECT * FROM USER;
+-- 
+DELETE FROM USER WHERE ROLLNUMBER='710019104002';
+SELECT * FROM USER;
+-- 
+DELETE FROM USER WHERE USERNAME='abi';
+SELECT * FROM USER;
